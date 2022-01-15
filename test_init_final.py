5c05e77b1b7b8262d3a4ea4d2b29c683ede667b2
kimsikyoul <97233108+kimsikyoul@users.noreply.github.com>
2022-01-15 11:40:03 -0500

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -1116,8 +1116,10 @@ class taskCog(commands.Cog):
 							bossFlag0[i] = True
 							if bossData[i][6] != '' :
 								await self.bot.get_channel(channel).send("```" + bossData[i][0] + ' ' + basicSetting[3] + 'minutes remaining ' + bossData[i][3] + " [" +  bossTimeString[i] + "]" + '\n<' + bossData[i][6] + '>```', tts=False)
+								await message.channel.send("@everyone" + bossData[i][0] + "5mins!")
 							else :
 								await self.bot.get_channel(channel).send("```" + bossData[i][0] + ' ' + basicSetting[3] + 'minutes remaining ' + bossData[i][3] + " [" +  bossTimeString[i] + "]```", tts=False)
+								await message.channel.send("@everyone" + bossData[i][0] + "5mins!")
 							try:
 								if basicSetting[21] == "1":
 									await PlaySound(self.bot.voice_clients[0], './sound/' + bossData[i][0] + '알림1.mp3')
@@ -1132,8 +1134,10 @@ class taskCog(commands.Cog):
 							bossFlag[i] = True
 							if bossData[i][6] != '' :
 								await self.bot.get_channel(channel).send("```" + bossData[i][0] + ' ' + basicSetting[1] + 'minutes ' + bossData[i][3] + " [" +  bossTimeString[i] + "]" + '\n<' + bossData[i][6] + '>```', tts=False)
+								await message.channel.send("@everyone" + bossData[i][0] + "1min!")
 							else :
 								await self.bot.get_channel(channel).send("```" + bossData[i][0] + ' ' + basicSetting[1] + 'minutes ' + bossData[i][3] + " [" +  bossTimeString[i] + "]```", tts=False)
+								await message.channel.send("@everyone" + bossData[i][0] + "1min!")
 							try:
 								if basicSetting[21] == "1":
 									await PlaySound(self.bot.voice_clients[0], './sound/' + bossData[i][0] + '알림.mp3')
